7fd5156b71465917098bc3cd10edd3ea93ed4c68
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-03-21 23:16:43 +0100

    Add box-shadow to widgetcard

--- src/components/Layout/Navigation/index.jsx
+++ src/components/Layout/Navigation/index.jsx
@@ -2,15 +2,16 @@ import HealthTracker from "../../../pages/HealthTracker/index";
 import HireTrainers from "../../../pages/HireTrainers/index";
 import WorkoutRoom from "../../../pages/WorkoutRoom/index";
 import Homepage from '../../../pages/homepage';
+import styles from "./nav.module.css"
 
 const Navigation = ({ setCurrentPage }) => {
   return (
-    <nav className="nav">
-      <li onClick={() => setCurrentPage(<Homepage />)}>Home</li>
+    <ul className={styles.navDesktop}>
+      <li onClick={() => setCurrentPage(<Homepage />)}>Dashboard</li>
       <li onClick={() => setCurrentPage(<HealthTracker />)}>Health Tracker</li>
-      <li onClick={() => setCurrentPage(<WorkoutRoom />)}>Fitness Room</li>
+      <li onClick={() => setCurrentPage(<WorkoutRoom />)}>Workout Room</li>
       <li onClick={() => setCurrentPage(<HireTrainers />)}>Hire trainers</li>
-    </nav>
+    </ul>
   );
 };
 export default Navigation;
