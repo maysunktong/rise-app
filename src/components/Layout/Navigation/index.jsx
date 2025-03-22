34dbf00e25ff900b076aa7c1cf21cb81f919fdb4
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-03-22 17:17:22 +0100

    Style nav

--- src/components/Layout/Navigation/index.jsx
+++ src/components/Layout/Navigation/index.jsx
@@ -1,17 +1,56 @@
+import { useState } from "react";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LuDumbbell } from "react-icons/lu";
+import { PiPaperPlaneTiltBold } from "react-icons/pi";
+import { RiHome5Fill } from "react-icons/ri";
+import Logo from "../../../assets/logo.svg";
 import HealthTracker from "../../../pages/HealthTracker/index";
 import HireTrainers from "../../../pages/HireTrainers/index";
 import WorkoutRoom from "../../../pages/WorkoutRoom/index";
-import Homepage from '../../../pages/homepage';
-import styles from "./nav.module.css"
+import Homepage from "../../../pages/homepage";
+import styles from "./nav.module.css";
 
 const Navigation = ({ setCurrentPage }) => {
+  const [activePage, setActivePage] = useState("dashboard");
+  const handleNavClick = (page, component) => {
+    setActivePage(page);
+    setCurrentPage(component);
+  };
+
   return (
-    <ul className={styles.navDesktop}>
-      <li onClick={() => setCurrentPage(<Homepage />)}>Dashboard</li>
-      <li onClick={() => setCurrentPage(<HealthTracker />)}>Health Tracker</li>
-      <li onClick={() => setCurrentPage(<WorkoutRoom />)}>Workout Room</li>
-      <li onClick={() => setCurrentPage(<HireTrainers />)}>Hire trainers</li>
-    </ul>
+    <div className={styles.navContainer}>
+      <div className={styles.logoContainer}>
+        <img src={Logo} alt="logo" width={200} />
+      </div>
+      <p className={styles.menuHeader}>Menu</p>
+      <ul className={styles.navDesktop}>
+        <li
+          className={activePage === "dashboard" ? styles.active : ""}
+          onClick={() => handleNavClick("dashboard", <Homepage />)}
+        >
+          <RiHome5Fill /> Dashboard
+        </li>
+        <li
+          className={activePage === "healthTracker" ? styles.active : ""}
+          onClick={() => handleNavClick("healthTracker", <HealthTracker />)}
+        >
+          <IoCalendarOutline /> Health Tracker
+        </li>
+        <li
+          className={activePage === "workoutRoom" ? styles.active : ""}
+          onClick={() => handleNavClick("workoutRoom", <WorkoutRoom />)}
+        >
+          <LuDumbbell /> Workout Room
+        </li>
+        <li
+          className={activePage === "hireTrainers" ? styles.active : ""}
+          onClick={() => handleNavClick("hireTrainers", <HireTrainers />)}
+        >
+          <PiPaperPlaneTiltBold /> Hire Trainers
+        </li>
+      </ul>
+    </div>
   );
 };
+
 export default Navigation;
